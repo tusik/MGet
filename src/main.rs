8943bebb2c9916c44d929ceb17e7a61ced176137
Image <image@by.cx>
2022-04-06 22:11:14 +0800

    write download data to file

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,9 @@ async fn main() {
     
     let u:String = "https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.17.1.tar.xz".parse().unwrap();
     let size = Downloader::get_range(u.clone()).await;
-    let down = Downloader::new(u.clone());
-
     print!("{}",size.unwrap());
+    let down = Downloader::new("linux-5.17.1.tar.xz".to_string()).await;
+    down.download(u).await;
+    
     // Downloader::range_check(u);
 }
--- src/mget/downloader.rs
+++ src/mget/downloader.rs
@@ -1,15 +1,18 @@
-use std::sync::{Arc, Mutex};
-use tokio::fs::{File, OpenOptions};
+use std::{sync::{Arc}, io::SeekFrom};
+use bytes::Bytes;
+use tokio::{fs::{File, OpenOptions}, io::{AsyncSeekExt, AsyncWriteExt}, sync::Mutex};
 
 pub struct Downloader{
     file_name:String,
-    file : Arc<Mutex<File>>
+    file : Arc<Mutex<File>>,
+    batch_size : u64
 }
 impl Downloader {
     pub async fn new(path:String) -> Downloader{
         Downloader{
             file_name:path.clone(),
-            file: Downloader::file(path).await
+            file: Downloader::file(path).await,
+            batch_size: 10240
         }
     }
     pub async fn file(path:String) ->Arc<Mutex<File>> {
@@ -43,6 +46,61 @@ impl Downloader {
                 panic!("{}",e);
             },
         }
+    }
+    pub async fn download_byte(url:String,start:u64,end:u64,file:Arc<Mutex<File>>)->Option<Bytes>{
+        let client = reqwest::Client::new();
+        let resp = client.get(url).header("Range", format!("bytes={}-{}",start,end)).send().await;
+        
+        match resp {
+            Ok(response) => {
+                let data = response.bytes().await.ok().unwrap();
+                Downloader::write_to_file(file, start, &data).await;
+                Some(data)
+
+            },
+            Err(e) => {
+                print!("{}",e);
+                return None
+            },
+        }
+    }
+    pub async fn write_to_file(file:Arc<Mutex<File>>,start:u64,data:&[u8]){
+        let mut file_p = file.lock().await;
+        file_p.seek(SeekFrom::Start(start as u64)).await.unwrap();
+        file_p.write_all(data).await;
+    }
+    pub async fn download(&self,url:String){
+        let mut handles = vec![];
+        let range_check = Downloader::get_range(url.to_string()).await;
+    
+        let mut index = 0;
+        let batch_size = 1024000;
+        if range_check.is_some() {
+            let max_size = range_check.unwrap();
+            while index*batch_size<max_size {
+                let f = self.file.clone();
+                let u = url.clone();
+                let download_batch_size = self.batch_size.clone();
+                handles.push(tokio::spawn(async move{
+                    let data = 
+                    Downloader::download_byte(
+                        u,
+                        index*download_batch_size, 
+                        std::cmp::min((index+1)*download_batch_size,max_size),
+                        f
+                    ).await.unwrap();
+                    // Downloader::write_to_file(f,index*download_batch_size,data.as_ref()).await;
+                })); 
+                
+                // inner_file.write_all(data.as_ref()).await;
+                index+=1;
+            }
+            
+        }else{
+            
+        }
+        futures::future::join_all(handles).await;
+        
     }
    
 }
