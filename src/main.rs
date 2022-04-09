35b4ee17c34067195149d911778998b96d611300
Image <image@by.cx>
2022-04-09 23:06:18 +0800

    first work available version

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,49 @@
-use MGet::mget::downloader::{self, Downloader};
+use mget::mget::downloader::Downloader;
+use clap::Parser;
+#[derive(Parser, Debug)]
+///A Mutil thread download tool 
+///https://github.com/tusik/MGet
+#[clap(author, version, about, long_about = None)]
+pub struct Args{
+    #[clap(short, long)]
+    url: String,
+    #[clap(short, long)]
+    file_name: Option<String>,
+    /// Data unit: byte
+    #[clap(short, long, default_value_t=1024*1024)]
+    batch_size: u64
+
+}
+
+pub async fn do_download(args:Args){
+    let u:String = args.url;
+    let down = Downloader::new(u).await;
+
+    match down {
+        Some(mut downloader) => {
+            if args.file_name.is_some(){
+                downloader.file(args.file_name.unwrap()).await;
+            }
+            downloader.batch_size(args.batch_size);
+            downloader.download().await;
+
+        },
+        None => {
+
+        },
+    }
+}
 #[tokio::main]
 async fn main() {
-    println!("Hello, MGet!");
-    
-    let u:String = "https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.17.1.tar.xz".parse().unwrap();
-    let size = Downloader::get_range(u.clone()).await;
-    print!("{}",size.unwrap());
-    let down = Downloader::new("linux-5.17.1.tar.xz".to_string()).await;
-    down.download(u).await;
-    
-    // Downloader::range_check(u);
+    let args = Args::parse();
+    do_download(args).await;
+}
+#[tokio::test]
+async fn main_test(){
+    let args = Args{ 
+        url: "https://mirrors.aliyun.com/debian-cd/current/amd64/log/20220326/B_amd64_dvd.log".to_string(), 
+        file_name: None, 
+        batch_size: 1024*100
+     };
+    do_download(args).await;
 }
--- src/mget/downloader.rs
+++ src/mget/downloader.rs
@@ -1,21 +1,43 @@
-use std::{sync::{Arc}, io::SeekFrom};
+use std::{sync::{Arc}, io::{SeekFrom, Read}};
 use bytes::Bytes;
+use reqwest::{Method, Url};
 use tokio::{fs::{File, OpenOptions}, io::{AsyncSeekExt, AsyncWriteExt}, sync::Mutex};
-
+use futures::{self, stream::FuturesUnordered, StreamExt};
 pub struct Downloader{
     file_name:String,
-    file : Arc<Mutex<File>>,
-    batch_size : u64
+    file: Arc<Mutex<File>>,
+    url: String,
+    batch_size : u64,
 }
 impl Downloader {
-    pub async fn new(path:String) -> Downloader{
-        Downloader{
-            file_name:path.clone(),
-            file: Downloader::file(path).await,
-            batch_size: 10240
-        }
+    pub async fn file(&mut self, file_path:String){
+        self.file_name = file_path.clone();
+        self.file = Downloader::open_file(file_path.clone()).await;
     }
-    pub async fn file(path:String) ->Arc<Mutex<File>> {
+    pub fn batch_size(&mut self, batch_size:u64){
+        self.batch_size = batch_size;
+    }
+    pub async fn new(url:String) -> Option<Downloader>{
+        let u = Url::parse(&url);
+        match u {
+            Ok(url_item) => {
+                let file_path = url_item.path().to_string(); 
+                let file_path:Vec<&str> = file_path.split("/").collect();
+                let file_os_file_name: String = file_path.last().unwrap().to_string();
+                Some(
+                    Downloader{
+                        file_name: file_os_file_name.clone(),
+                        file: Downloader::open_file(file_os_file_name.clone()).await,
+                        batch_size: 1024000,
+                        url
+                    }
+                )
+            },
+            Err(_) => None,
+        }
+        
+    }
+    pub async fn open_file(path:String) ->Arc<Mutex<File>> {
         let mut op = OpenOptions::new();
         let file = Arc::new(Mutex::new(op.read(true)
             .write(true)
@@ -25,11 +47,17 @@ impl Downloader {
     }
     pub async fn get_range(url:String) -> Option<u64>{
         let client = reqwest::Client::new();
-        let rep = client.head(url).send().await;
+        
+        let rep = client.request(Method::HEAD, url).header("user-agent", "Mget-rs").send().await;
         match rep {
             Ok(response) => {
-                let a_r = &response.headers().get("Accept-Ranges");
-                let size = &response.headers().get("Content-Length").unwrap().to_str()
+                let head = &response.headers();
+                let a_r = head.get("Accept-Ranges");
+                let mut head_size = head.get("content-length");
+                if head_size.is_none(){
+                    head_size = head.get("Content-Length");
+                }
+                let size = head_size.unwrap().to_str()
                         .map_or(0, |v|v.parse::<u64>().map_or(0, |s|s));
                 match a_r {
                     Some(v) => {
@@ -49,14 +77,17 @@ impl Downloader {
     }
     pub async fn download_byte(url:String,start:u64,end:u64,file:Arc<Mutex<File>>)->Option<Bytes>{
         let client = reqwest::Client::new();
-        let resp = client.get(url).header("Range", format!("bytes={}-{}",start,end)).send().await;
+        let resp = client.get(url)
+            .header("Range", format!("bytes={}-{}",start,end))
+            .header("user-agent", "Mget-rs")
+            .send().await;
         
         match resp {
             Ok(response) => {
                 let data = response.bytes().await.ok().unwrap();
+                println!("{}-{}",start,end);
                 Downloader::write_to_file(file, start, &data).await;
                 Some(data)
-
             },
             Err(e) => {
                 print!("{}",e);
@@ -64,42 +95,46 @@ impl Downloader {
             },
         }
     }
-    pub async fn write_to_file(file:Arc<Mutex<File>>,start:u64,data:&[u8]){
+    pub async fn write_to_file(file:Arc<Mutex<File>>,start:u64,data:&Bytes){
         let mut file_p = file.lock().await;
         file_p.seek(SeekFrom::Start(start as u64)).await.unwrap();
-        file_p.write_all(data).await;
+        let byte_data: Result<Vec<_>, _> = data.bytes().collect();
+        let byte_data = byte_data.expect("Unable to read data");
+        file_p.write_all(&byte_data).await.expect("Unable to write data");
     }
-    pub async fn download(&self,url:String){
-        let mut handles = vec![];
-        let range_check = Downloader::get_range(url.to_string()).await;
-    
+    pub async fn download(&mut self){
+        
+        let range_check = Downloader::get_range(self.url.to_string()).await;
+        let mut futs = FuturesUnordered::new();
         let mut index = 0;
-        let batch_size = 1024000;
         if range_check.is_some() {
             let max_size = range_check.unwrap();
-            while index*batch_size<max_size {
+            while index * self.batch_size < max_size {
                 let f = self.file.clone();
-                let u = url.clone();
+                let u = self.url.clone();
                 let download_batch_size = self.batch_size.clone();
-                handles.push(tokio::spawn(async move{
-                    let data = 
-                    Downloader::download_byte(
-                        u,
-                        index*download_batch_size, 
-                        std::cmp::min((index+1)*download_batch_size,max_size),
-                        f
-                    ).await.unwrap();
-                    // Downloader::write_to_file(f,index*download_batch_size,data.as_ref()).await;
-                })); 
-                
-                // inner_file.write_all(data.as_ref()).await;
+                let task = tokio::spawn(async move {
+                        Downloader::download_byte(
+                            u,
+                            index*download_batch_size, 
+                            std::cmp::min((index+1) * download_batch_size,max_size),
+                            f
+                        ).await;
+                });
+                futs.push(task); 
+                if futs.len() == 3 {
+                    futs.next().await;
+                }
                 index+=1;
             }
-            
+            while let Some(_) = futs.next().await {
+                // outputs.push(item);
+            }
+                        
         }else{
             
         }
-        futures::future::join_all(handles).await;
+
         
     }
    
